09824e9ac42539a7a95b60dcac5e4d07a609afc5
Rajkumar <87382849+rajoffl@users.noreply.github.com>
2021-08-20 23:45:35 +0530

    Add files via upload

---
+++ SQL/new Companies.sql
@@ -0,0 +1,8 @@
+select  c.company_code,c.founder,
+(select count(*) from Lead_Manager where company_code=c.company_code  ) ,
+(select count(*) from Senior_Manager where company_code=c.company_code  ),
+(select count(*) from Manager where company_code=c.company_code  ),
+(select count(*) from Employee  where company_code=c.company_code  )
+
+
+from  Company as c order by c.company_code
