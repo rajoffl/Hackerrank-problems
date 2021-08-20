29489bf7602fc8661c03bb2216fa74dbd7783fd8
Rajkumar <87382849+rajoffl@users.noreply.github.com>
2021-08-21 00:05:45 +0530

    Delete new Companies.sql

--- new Companies.sql
+++
@@ -1,8 +0,0 @@
-SELECT c.company_code, c.founder, COUNT(DISTINCT l.lead_manager_code), COUNT(DISTINCT s.senior_manager_code), COUNT(DISTINCT m.manager_code), COUNT(DISTINCT e.employee_code)
-FROM Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e
-WHERE c.company_code = l.company_code
-     AND l.company_code = s.company_code
-     AND s.company_code = m.company_code
-     AND m.company_code = e.company_code
-GROUP BY c.company_code, c.founder
-ORDER BY c.company_code;
